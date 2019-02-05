d3c87509b6b0770d36c75c66ad2cf3298873be60
Laura Green <LGreen@spartaglobal.com>
2019-02-05 21:49:53 +0000

    apt-update added

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+apt_update 'mongodb'
 include_recipe "apt"
 apt_repository 'mongodb' do
   uri        'http://repo.mongodb.org/apt/ubuntu'
